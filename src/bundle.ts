6e064db882fcd8877269d2bcc06aee210a419ca1
Ravoan <emravoan1998@gmail.com>
2022-11-13 00:43:00 +0700

    Add Icons

--- src/bundle.ts
+++ src/bundle.ts
@@ -11,18 +11,24 @@ export default class KonToast {
       );
     }
 
+    const icons = {
+      error: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zm32 224c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/></svg>',
+      warning: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224c0-17.7-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32s32-14.3 32-32z"/></svg>',
+      success: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>',
+    }
+
     // prettier-ignore
     const dom = `
-      <div class="toast-header ${option.type == 'success' ? 'bg-success' : option.type == 'warning' ? 'bg-warning' : 'bg-danger'} text-white">
+      <div class="toast-header ${option.type == 'success' ? 'bg-success' : option.type == 'warning' ? 'bg-warning' : 'bg-danger'}">
         <div class="hstack gap-1 flex-fill">
-          <i class="fa-solid fa-${option.type == 'success' ? 'circle-check' : option.type == 'warning' ? 'triangle-exclamation' : 'circle-exclamation'} fs-4"></i>
-          <span class="fw-bold text-uppercase fs-7">${option.title}</span>
+          ${option.type == 'success' ? icons.success : option.type == 'warning' ? icons.warning : icons.error}
+          <span>${option.title}</span>
         </div>
         <div class="hstack">
           <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast"></button>
         </div>
       </div>
-      <div class="toast-body text-dark text-center fw-bold" style="backdrop-filter: blur(2px); line-height: 1.4;">${option.text}</div>
+      <div class="toast-body">${option.text}</div>
     `;
 
     const elToast = document.createElement('div');
